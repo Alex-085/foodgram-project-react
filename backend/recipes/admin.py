6be275abee2367959940694ce007d88f95883b07
Alexander <kudriavtcevs@bk.ru>
2024-01-21 23:52:36 +0300

    foodgram_test_1

---
+++ backend/recipes/admin.py
@@ -0,0 +1,67 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin import display
+from .models import (Subscribe,
+                     User,
+                     Favourite,
+                     Ingredient,
+                     IngredientInRecipe,
+                     Recipe,
+                     ShoppingCart,
+                     Tag)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'author', 'added_in_favorites')
+    readonly_fields = ('added_in_favorites',)
+    list_filter = ('author', 'name', 'tags',)
+
+    @display(description='Количество в избранных')
+    def added_in_favorites(self, obj):
+        return obj.favorites.count()
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'unit_of_measurement',)
+    list_filter = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug',)
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe',)
+
+
+@admin.register(Favourite)
+class FavouriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe',)
+
+
+@admin.register(IngredientInRecipe)
+class IngredientInRecipe(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount',)
+
+
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = (
+        'username',
+        'id',
+        'email',
+        'first_name',
+        'last_name',
+    )
+    list_filter = ('email', 'first_name')
+
+
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author',)
